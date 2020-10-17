81662129c818583c49de3ecac2b2c1aa1b08324d
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2020-10-17 19:46:41 +0200

    ODTable : reprise mise en forme pour utilisation de width et classe bootstrap 4, mise en commentaire ressource odselect.js car pas encore crée formatWidthBT : si valeur numérique seule, pas de calcul d'offset automatique (modification reportée dans OObject)

--- view/graphicobjecttemplating/oobjects/odcontained/odtable/odtable.rscs.php
+++ view/graphicobjecttemplating/oobjects/odcontained/odtable/odtable.rscs.php
@@ -6,7 +6,7 @@ return [
     ],
     'js'		=> [
         'odtable.js'    => 'js/odtable.js',
-        'odselect.js'   => '../odselect/js/odselect.js',
+//        'odselect.js'   => '../odselect/js/odselect.js',
         'odbutton.js'   => '../odbutton/js/odbutton.js',
     ],
 ];
