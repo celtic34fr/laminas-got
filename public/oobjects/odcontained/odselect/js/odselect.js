e011225ec32199252abd0a25ad963b03b6e50f22
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2020-11-30 11:05:45 +0100

    ODSelect rédaction du premier jet de la transcription ancienne version vers nouvelle avec éclatement en GOT et LaminasGOT + modifications lègères non structurelles

---
+++ public/oobjects/odcontained/odselect/js/odselect.js
@@ -0,0 +1,44 @@
+
+class odselect {
+    constructor(obj) {
+        this.id     = obj.attr('id');
+        this.form   = obj.data('form');
+        this.objet  = obj.data('objet');
+        this.data   = obj.data();
+    }
+
+    getData(evt) {
+        let selected = '';
+        $('#'+this.id+' option:selected').each(function () { selected += $(this).val() + "-" });
+        selected = selected.substring(0, selected.length - 1);
+        return {id : this.id, value: selected, event:"change", object : this.objet};
+    }
+
+    setData(data) {
+        $.each(data, function (i, value) {
+            $('#'+this.id+' option[value='+value+']').attr('selected','selected');
+        });
+    }
+
+    invalidate() {
+        // à rédiger ????
+    }
+}
+
+jQuery(document).ready(function (evt) {
+
+    $(document).on('change', ".gotObject.selectChg", function (evt) {
+        let objet = new odselect($(this));
+        let invalid = "";
+        if (typeof objet.invalidate === "function") { invalid = objet.invalidate(); }
+        if (invalid.length === 0) {
+            $(this).remove("has-error");
+            $(this).find("span").removeClass("hidden").addClass("hidden");
+            invokeAjax(objet.getData("click"), $(this).attr("id"), "click", evt);
+        } else {
+            $(this).remove("has-error").addClass("has-error");
+            $(this).find("span").removeClass("hidden").html(invalid);
+        }
+    });
+
+});
---
+++ view/graphicobjecttemplating/oobjects/odcontained/odselect/odselect.rscs.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'css'       => [
+        'dropdown.css' => 'css/dropdown.css',
+    ],
+    'js'		=> [
+        'odselect.js' => 'js/odselect.js',
+        'dropdown.js' => 'js/dropdown.js',
+    ],
+];
